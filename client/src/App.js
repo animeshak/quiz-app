0f2b59b84b12ad99f43ca11332b43f071cf0575f
Animesh Kambhale <animeshkambhale2001@gmail.com>
2024-05-11 00:23:27 +0530

    sign in form

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,17 @@ import {RouterProvider, createBrowserRouter} from "react-router-dom"
 import './App.css';
 import AdminPanel from './Components/AdminPanel';
 import Quiz from './Components/Quiz';
+import SignIn from "./Components/login/SignIn";
 
 
 
 function App() {
 
   const route = createBrowserRouter([
+    {
+      path: "/",
+      element: <SignIn/>
+    },
     {
       path: "/admin",
       element: <AdminPanel/>
